f07c7610b9155eb20487facd70bc129632cea66d
PrateekDiwaker <109203386+PrateekDiwaker@users.noreply.github.com>
2022-07-17 15:13:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+ $('.clicks').on('click',function(){
+     $('#box').addClass('show');
+       
+ })
+ $('.submission').on('click',function(){
+     $('#box').removeClass('show');
+ });
+ $('.closex').on('click',function(){
+    $('#box').removeClass('show');
+ })
+});
+
